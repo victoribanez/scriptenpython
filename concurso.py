02bc173aa84808bfff3f0a7fa2901a344b7092ff
Daniel <dsanmartinreyes@gmail.com>
2020-09-28 13:52:48 -0300

    Fixed indentation

--- concurso.py
+++ concurso.py
@@ -4,14 +4,14 @@ import csv
 
 participantes = []
 with open('participantes.csv', 'r') as csvfile:
-	p = csv.reader(csvfile)
-	for row in p:
-		participantes.append(row[1])
+    p = csv.reader(csvfile)
+    for row in p:
+        participantes.append(row[1])
 
 texto = ["El/la primere al agua: ", "El/la segunde al agua: ", 
-	"El/la tercere al agua: ", "El/la afortunade es... "]
+    "El/la tercere al agua: ", "El/la afortunade es... "]
 
 # Tres al agua y el ganador
 for i in range(4):
-	print("%s %s" % (texto[i], participantes[random.randint(0, len(participantes))]))
-	input()
+    print("%s %s" % (texto[i], participantes[random.randint(0, len(participantes))]))
+    input()
