da54715a5105a593ec90c342c51a162009b6254d
Daniel <dsanmartinreyes@gmail.com>
2020-09-28 13:51:05 -0300

    Change raw_input() to input()

--- concurso.py
+++ concurso.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-import random, csv
+import random
+import csv
 
 participantes = []
-with open('participantes.csv', 'rb') as csvfile:
+with open('participantes.csv', 'r') as csvfile:
 	p = csv.reader(csvfile)
 	for row in p:
 		participantes.append(row[1])
@@ -13,4 +14,4 @@ texto = ["El/la primere al agua: ", "El/la segunde al agua: ",
 # Tres al agua y el ganador
 for i in range(4):
 	print("%s %s" % (texto[i], participantes[random.randint(0, len(participantes))]))
-	raw_input()
+	input()
